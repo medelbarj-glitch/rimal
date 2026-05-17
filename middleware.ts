a9232dd05ce39726e26b211b63e4b4bcf4d4c383
Yanisdiga <gaidiyanis@gmail.com>
2026-05-17 23:49:36 +0200

    update comment

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,5 @@ export default createMiddleware(routing);
 export const config = {
   // Le "matcher" indique à Next.js quelles routes doivent passer par ce middleware.
   // Il va intercepter la racine (/) et toutes les routes contenant tes langues.
-  // Il ignore les fichiers statiques (images, css) et les API.
   matcher: ['/', '/(fr|en|es|ar)/:path*']
 };
